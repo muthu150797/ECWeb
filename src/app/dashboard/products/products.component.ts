541d0a3263efc5d361c5aa93651ea194b8d8ffaf
muthu mani <muthumani150797@gmail.com>
2022-06-27 14:12:53 +0530

    product list in table

--- src/app/dashboard/products/products.component.ts
+++ src/app/dashboard/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { ResponseModel } from 'src/app/Model/ResponseModel';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from 'src/app/shop/shop.service';
 import { ProductService } from '../ProductService';
@@ -11,20 +13,118 @@ import { ProductService } from '../ProductService';
 export class ProductsComponent implements OnInit {
   products:any;
   totalCount: number;
-
-  constructor(private productService:ProductService) { }
+  first = 0;
+  rows = 10;
+  clonedProducts: { [s: string]: IProduct } = {};
+  loading: boolean = true;
+  productList: any;
+  cols: { field: string; header: string; }[];
+  contentId: any;
+  display=false;
+  contentName='';
+  header: string;
+  popup: boolean;
+  constructor(private toastr:ToastrService,private productService:ProductService) { }
 
   ngOnInit(): void {
-    this.GetAllProducts()
+    this.GetAllProducts();
+    this.cols = [
+      { field: 'id', header: 'Id' },
+      { field: 'name', header: 'Name' },
+      { field: 'description', header: 'Description' },
+      { field: 'price', header: 'Price' },
+      { field: 'productBrand', header: 'Product Brand' },
+      { field: 'productType', header: 'Product Type' },
+
+      // { field: 'email', header: 'Email' },
+    ];
+  }
+
+  onRowEditInit(product: any) {
+    this.clonedProducts[product.id] = { ...product };
+  }
+  onRowEditSave(product: any) {
+    if (product.id > 0) {
+      delete this.clonedProducts[product.id];
+      this.productService.AddOrUpdateProductType(product).subscribe(
+        (res: ResponseModel) => {
+          console.log('response from updating product', res);
+          if (res.statusCode == 200) {
+            this.toastr.success(res.message);
+            this.GetAllProducts();
+          } else {
+            this.toastr.error(res.message);
+          }
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else {
+      this.toastr.error("Not Found");
+    }
+  }
+  onRowEditCancel(product: any, index: number) {
+    this.productList[index] = this.clonedProducts[product.id];
+    delete this.clonedProducts[product.id];
+  }
+  next() {
+    this.first = this.first + this.rows;
+  }
+
+  prev() {
+    this.first = this.first - this.rows;
+  }
+
+  reset() {
+    this.first = 0;
+  }
+
+  isLastPage(): boolean {
+    return this.productList
+      ? this.first === this.productList.length - this.rows
+      : true;
+  }
+
+  isFirstPage(): boolean {
+    return this.productList ? this.first === 0 : true;
   }
   GetAllProducts()
   {
     this.productService.GetAllProducts().subscribe(response => {
       console.log("All Products",response);
-     this.products = response;
+     this.productList = response;
       //this.totalCount = response.count;
     }, error => {
       console.log(error);
     });
+  }
+  showDialog1(content){
+    this.display=true;
+    this.header='Delete Product';
+    this.contentName="Are you sure want to delete the "+content.name+"?";
+    this.contentId=content.id;
+  }
+  AddProduct(){}
+  DeleteProduct(productId){
+    this.productService.DeleteProduct(productId).subscribe(
+      (res: any) => {
+        console.log('response from deleting product', res);
+        if (res.statusCode == 200) {
+          this.toastr.success(res.message);
+          this.GetAllProducts();
+        } else {
+          this.GetAllProducts();
+          this.toastr.error(res.message);
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    this.contentName='';
+    this.contentId=0;
+    this.header='';
+    this.display=false;
   }
 }
